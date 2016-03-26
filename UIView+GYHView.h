8a5b57b18ec555c325ae24cd51efb440b2f2a50b
Goyohol <qianfeng@chengduqianfengdeMac-mini.local>
2016-03-26 10:00:03 +0800

    UIView's animations
    UIView’s animations put together in this library

---
+++ UIView+GYHView.h
@@ -0,0 +1,38 @@
+//
+//  UIView+GYHView.h
+//模态控制器切换
+//
+//  Created by 千锋 on 16/2/23.
+//  Copyright © 2016年 龚雨豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    GYHTransitionFade,  //交叉淡化过渡
+    GYHTransitionMoveIn,   //新视图移到旧视图上面
+    GYHTransitionPush, //新视图把旧视图推出去
+    GYHTransitionReveal,//将旧视图移开,显示下面的新视图
+    GYHTransitionpageCurl,//向上翻一页
+    GYHTransitionpageUnCurl,//向下翻一页
+    GYHTransitionRippleEffect,//滴水效果
+    GYHTransitionSuckEffect,//收缩效果，如一块布被抽走
+    GYHTransitionCube,//立方体效果
+    GYHTransitionOglFlip,//上下翻转效果
+    GYHTransitionCameraIrisHollowOpen,//打开照相机效果
+    GYHTransitionCameraIrisHollowClose//关闭相机效果
+} GYHTransitionType;//❤️ 枚举名字
+
+
+typedef enum : NSUInteger{  //运动方向
+    GYHTransitionFromRight,
+    GYHTransitionFromLeft,//(默认值)
+    GYHTransitionFromTop,
+    GYHTransitionFromBottom
+} GYHTransitionDirection;
+
+@interface UIView(GYHView)
+//参数：都是自己定义的枚举类型❤️
+-(void)addTransitionAnimationWithDuration:(double)duration type:(GYHTransitionType)transitionType Directiontype:(GYHTransitionDirection)direction;
+
+@end
---
+++ UIView+GYHView.m
@@ -0,0 +1,30 @@
+//
+//  UIView+GYHView.m
+//模态控制器切换
+//
+//  Created by 千锋 on 16/2/23.
+//  Copyright © 2016年 龚雨豪. All rights reserved.
+//
+
+#import "UIView+GYHView.h"
+
+@implementation UIView(GYHView)
+//重写 动画方法❤️
+-(void)addTransitionAnimationWithDuration:(double)duration type:(GYHTransitionType)transitionType Directiontype:(GYHTransitionDirection)direction
+{
+    //1.创建一个切换 动画对象
+    CATransition * transition = [CATransition animation];
+    
+    //2.设置 动画时间
+    transition.duration = duration;
+    //3.设置 动画类型
+    NSArray * typeArray = @[kCATransitionFade,kCATransitionMoveIn,kCATransitionPush,kCATransitionReveal,@"pageCurl",@"pageUnCurl",@"rippleEffect",@"suckEffect",@"cube",@"oglFlip",@"cameraIrisHollowOpen",@"cameraIrisHollowClose"];
+    transition.type = typeArray[transitionType];
+    //4.设置动画方向
+    NSArray * directionArray = @[kCATransitionFromRight,kCATransitionFromLeft,kCATransitionFromTop,kCATransitionFromBottom];
+    transition.subtype = directionArray[direction];
+    //5.添加动画()
+    [self.window.layer addAnimation:transition forKey:nil];
+}
+
+@end
